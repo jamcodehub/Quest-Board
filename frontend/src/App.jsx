dc099cfb7dff2041981149086f64eaa65a64b0a0
James <145176861+jamcodehub@users.noreply.github.com>
2026-08-04 13:18:35 +1000

    folder restructure and tweeks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import GhostDialogue from './components/GhostDialogue';
+import Navbar from '../components/Navbar';
+import GhostDialogue from '../components/GhostDialogue';
 
 // --- HOME / STUDENT VIEW COMPONENT ---
 function StudentPortal() {
